9bd8543f69f915337fdc24dc1ccb8f25d32d1d3d
sham1316 <sham1316@gmail.com>
2021-01-18 15:01:40 +0000

    update builder

--- build.sh
+++ build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
-SSH_USER = $1
-SSH_HOST = $2
-SSH_HOST_DEV = $3
-SSH_PASS = $4
-BRANCH = $5
-DEPLOY_FOLDER = $6
+export BRANCH=$(echo $JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
+export TAG="$BRANCH-s$JB_SPACE_EXECUTION_NUMBER"
 
-env
+if [ "$BRANCH" = "master" ]; then 
+    echo "master" 
+	export REMOTE_HOST=$SSH_HOST
+else
+    echo "not master"  
+	export REMOTE_HOST=$SSH_HOST_DEV
+fi;
+
 echo "adding host to known host"
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts 
-ssh-keyscan $SSH_HOST >> ~/.ssh/known_hosts 
+ssh-keyscan $REMOTE_HOST >> ~/.ssh/known_hosts 
 echo "run command on remote server"
 
-sshpass -p pass ssh ${2}@sftp  << EOF
-    ls
-    pwd
+sshpass -p $SSH_PASS ssh ${SSH_USER}@${REMOTE_HOST}  << EOF
+	cd $DEPLOY_FOLDER && \
+    sed -i 's/${TAG_NAME}=.*/${TAG_NAME}=${TAG}/' .env && \
+    docker-compose up -d && \
+    docker image  prune -af
 EOF
